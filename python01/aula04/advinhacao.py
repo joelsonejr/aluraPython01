f3adb153d197db1538bb2e2ec7c6d761a13d4c44
Joelson Junior <joelson.ejr@gmail.com>
2023-06-07 15:56:23 -0300

    incremented the code using the format function and a while loop

--- python01/aula04/advinhacao.py
+++ python01/aula04/advinhacao.py
@@ -3,19 +3,31 @@ print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
 secret_number = 42
+attempts = 3
+rounds = 1
 
-user_guess = int(input("Digite o seu número: "))
+while (rounds <= attempts):
 
-print("Você digitou: ", user_guess)
+    print("Tentativa {} de {}".format(rounds,attempts))
 
-guessed_correctly = secret_number == user_guess
-guessed_more = secret_number < user_guess
-guessed_less = secret_number > user_guess
+    user_guess = int(input("Digite o seu número: "))
 
-if guessed_correctly:
-    print("Você acertou")
-else:
-    if guessed_more:
-        print("Você errou. O seu chute foi maior do que o número secreto")
-    elif guessed_less:
-        print("Você errou. O seu chute foi menor do que o número secreto")
+    print("Você digitou: ", user_guess)
+
+    guessed_correctly = secret_number == user_guess
+    guessed_more = secret_number < user_guess
+    guessed_less = secret_number > user_guess
+
+
+
+    if guessed_correctly:
+        print("Você acertou")
+        break
+
+    else:
+        if guessed_more:
+            print("Você errou. O seu chute foi maior do que o número secreto")
+        elif guessed_less:
+            print("Você errou. O seu chute foi menor do que o número secreto")
+
+    rounds = rounds +1
