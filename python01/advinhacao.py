0c57a937e00f468a8e75337c617645586d76cdea
Joelson Junior <joelson.ejr@gmail.com>
2023-07-14 21:40:25 -0300

    finished first module

--- python01/advinhacao.py
+++ python01/advinhacao.py
@@ -1,56 +1,61 @@
+
 import random
 
-print("*********************************")
-print("Bem vindo ao jogo de Adivinhação!")
-print("*********************************")
+def playAdivinhacao():
 
-secret_number = random.randrange(1, 101)
-max_attempts = 3
-rounds = ""
-points = 1000
 
-print("Selecione o nível de dificuldade desejado: ")
-print("(1) Fácil (2) Médio (3) Difícil")
+    print("*********************************")
+    print("Bem vindo ao jogo de Adivinhação!")
+    print("*********************************")
 
-gameDificulty = int(input("Nível de dificuldade: "))
-
-if (gameDificulty == 1):
-    max_attempts = 10
-elif (gameDificulty == 2):
-        max_attempts = 5
-else:
+    secret_number = random.randrange(1, 101)
     max_attempts = 3
+    rounds = ""
+    points = 1000
 
+    print("Selecione o nível de dificuldade desejado: ")
+    print("(1) Fácil (2) Médio (3) Difícil")
 
-for rounds in range(1, max_attempts + 1):
+    gameDificulty = int(input("Nível de dificuldade: "))
 
-    print("Tentativa {} de {}".format(rounds,max_attempts))
-
-    user_guess = int(input("Digite o seu número: "))
-
-    if user_guess <= 0:
-        while user_guess < 1 or user_guess > 100:
-            print("\n Seu palpite deve ser um número entre 1 e 100 \n")
-            user_guess = int(input("Digite o seu número: \n"))
-
-    print("\n Você digitou: ", user_guess)
-
-    guessed_correctly = secret_number == user_guess
-    guessed_more = secret_number < user_guess
-    guessed_less = secret_number > user_guess
-
-    if guessed_correctly:
-        print("Você acertou e fez {} pontos".format(points))
-        break
-
+    if (gameDificulty == 1):
+        max_attempts = 10
+    elif (gameDificulty == 2):
+            max_attempts = 5
     else:
-        if guessed_more:
-            print("Você errou. O seu chute foi maior do que o número secreto")
-        elif guessed_less:
-            print("Você errou. O seu chute foi menor do que o número secreto")
-        
-        lostPoints = abs(secret_number - user_guess)
-        points = points - lostPoints
+        max_attempts = 3
 
 
+    for rounds in range(1, max_attempts + 1):
 
+        print("Tentativa {} de {}".format(rounds,max_attempts))
+
+        user_guess = int(input("Digite o seu número: "))
+
+        if user_guess <= 0:
+            while user_guess < 1 or user_guess > 100:
+                print("\n Seu palpite deve ser um número entre 1 e 100 \n")
+                user_guess = int(input("Digite o seu número: \n"))
+
+        print("\n Você digitou: ", user_guess)
+
+        guessed_correctly = secret_number == user_guess
+        guessed_more = secret_number < user_guess
+        guessed_less = secret_number > user_guess
+
+        if guessed_correctly:
+            print("Você acertou e fez {} pontos".format(points))
+            break
+
+        else:
+            if guessed_more:
+                print("Você errou. O seu chute foi maior do que o número secreto")
+            elif guessed_less:
+                print("Você errou. O seu chute foi menor do que o número secreto")
+            
+            lostPoints = abs(secret_number - user_guess)
+            points = points - lostPoints
+
+# runs the program when it's executed directly from terminal, and not whem it's imported by another script.
+if __name__ == "__main__": 
+    playAdivinhacao()
